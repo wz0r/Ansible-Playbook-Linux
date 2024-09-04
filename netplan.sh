5526f937819ef0258ffc082f0bf45aec64994940
spd-it <spd-it@svod-int.ru>
2024-09-04 15:16:26 +0300

    Update netplan.sh

--- netplan.sh
+++ netplan.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ip=10.80.6.122
-
 gw=10.80.6.1
-
 interface=ens192
 
 cd /etc/default/
@@ -13,7 +11,6 @@ sudo update-grub
 sudo apt update
 sudo apt install mc ccze ifupdown net-tools resolvconf curl -y
 
-#cd /etc/network/interfaces.d/
 cat << EOF > /etc/network/interfaces.d/$interface
 auto $interface
 iface $interface inet static
@@ -25,7 +22,6 @@ dns-nameservers 10.80.3.2,10.99.3.2 8.8.8.8 8.8.4.4
 EOF
 
 sudo unlink /etc/resolv.conf
-#cd /etc/
 
 cat << EOF >> /etc/resolv.conf
 nameserver 10.80.3.2
